8c945943cefa3645829aec9000ba99ec2a6191c5
Terer <terermike3@gmail.com>
2023-04-27 23:24:46 +0000

    Added a solution

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
+"""
+   method that determines if all the boxes can be opened
+   if an n number of locked boxes, Each box numbered sequentially
+   0 to n - 1 and each box may contain keys to the other boxes.
+"""
+
 
 def canUnlockAll(boxes):
-    n = len(boxes)
-    keys = set(boxes[0])  # start with the keys from the first box
-    unlocked = {0}  # start with the first box unlocked
-
-    while len(unlocked) < n:
-        found_new_box = False
-        for i in range(n):
-            if i in unlocked:
-                # this box is already unlocked, skip it
-                continue
-            if any(key in unlocked for key in boxes[i]):
-                # we have a key for this box, unlock it and add its keys to our keyring
-                unlocked.add(i)
-                keys.update(boxes[i])
-                found_new_box = True
-        if not found_new_box:
-            # we couldn't find any new boxes to unlock
-            return False
-
-    # we have unlocked all the boxes
-    return True
+    """
+        Parameter: boxes
+        Return: True or False
+    """
+    if not boxes or type(boxes) is not list:
+        return False
+    unlocked = [0]
+    for x in unlocked:
+        for y in boxes[x]:
+            if y not in unlocked and y < len(boxes):
+                unlocked.append(y)
+    return len(unlocked) == len(boxes)
