49b23468c16b0e85dc9591d5f65b3db31ae5e131
Terer <terermike3@gmail.com>
2023-05-18 13:50:22 +0300

    Corrected the solution

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
+"""UTF-8 Validation"""
 
 
 def validUTF8(data):
-    num_bytes = 0
+    """Determines if a given data set represents a valid UTF-8 encoding"""
+    bytes_remaining = 0
 
     for byte in data:
-        # Check if the current byte is a continuation byte
-        if num_bytes == 0:
-            # Count the number of bytes required for the current character
-            if byte >> 7 == 0b0:
-                num_bytes = 1
-            elif byte >> 5 == 0b110:
-                num_bytes = 2
-            elif byte >> 4 == 0b1110:
-                num_bytes = 3
-            elif byte >> 3 == 0b11110:
-                num_bytes = 4
-            else:
+        binary_byte = format(byte, '08b')
+
+        if bytes_remaining == 0:
+            for bit in binary_byte:
+                if bit == '0':
+                    break
+                bytes_remaining += 1
+
+            if bytes_remaining == 0:
+                continue
+
+            if bytes_remaining == 1 or bytes_remaining > 4:
                 return False
         else:
-            # Check if the byte is a continuation byte
-            if byte >> 6 != 0b10:
+            if not (binary_byte[0] == '1' and binary_byte[1] == '0'):
                 return False
 
-        # Decrement the byte count for the current character
-        num_bytes -= 1
+        bytes_remaining -= 1
 
-    # Ensure that all characters were properly terminated
-    return num_bytes == 0
+    return bytes_remaining == 0
