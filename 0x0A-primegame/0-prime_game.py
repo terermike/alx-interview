f61a3b3b683f5d7ef19175112ea0cfc7dbd1c1d1
Terer <terermike3@gmail.com>
2023-06-30 00:00:43 +0300

    Made corrections

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,40 +2,37 @@
 
 
 def isWinner(x, nums):
-    def is_prime(n):
-        if n <= 1:
-            return False
-        for i in range(2, int(n ** 0.5) + 1):
-            if n % i == 0:
-                return False
-        return True
-
-    wins = {"Maria": 0, "Ben": 0}
-
-    for n in nums:
-        primes = [num for num in range(2, n + 1) if is_prime(num)]
-        maria_turn = True
-
-        while primes:
-            if maria_turn:
-                player = "Maria"
-            else:
-                player = "Ben"
-
-            max_prime = max(primes)
-            primes.remove(max_prime)
-
-            for num in range(max_prime, n + 1, max_prime):
-                if num in primes:
-                    primes.remove(num)
-
-            maria_turn = not maria_turn
-
-        wins[player] += 1
-
-    if wins["Maria"] > wins["Ben"]:
-        return "Maria"
-    elif wins["Maria"] < wins["Ben"]:
-        return "Ben"
-    else:
+    """Function that checks for the winner"""
+    if not nums or x < 1:
         return None
+
+    max_num = max(nums)
+    is_prime = [True] * (max(max_num + 1, 2))
+
+    for i in range(2, int(max_num ** 0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, max_num + 1, i):
+                is_prime[j] = False
+
+    is_prime[0] = is_prime[1] = False
+
+    prime_count = [0] * len(is_prime)
+    prime_sum = 0
+
+    for i in range(len(is_prime)):
+        if is_prime[i]:
+            prime_sum += 1
+        prime_count[i] = prime_sum
+
+    maria_wins = 0
+
+    for num in nums:
+        if prime_count[num] % 2 == 1:
+            maria_wins += 1
+
+    if maria_wins * 2 == len(nums):
+        return None
+    elif maria_wins + 2 > len(nums):
+        return "Maria"
+    else:
+        return "Ben"
